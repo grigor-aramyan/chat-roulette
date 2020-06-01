ae3eae521e17113b9c6d321f772ca33e97ecbe58
grigor-aramyan <grigori.aramyan@gmail.com>
2020-06-01 20:29:02 +0400

    add firebase realtime database

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -102,6 +102,30 @@
             <example-component></example-component>
         </div>
 
+        <!-- The core Firebase JS SDK is always required and must be listed first -->
+        <script src="https://www.gstatic.com/firebasejs/7.14.6/firebase-app.js"></script>
+
+        <!-- TODO: Add SDKs for Firebase products that you want to use
+            https://firebase.google.com/docs/web/setup#available-libraries -->
+        <script src="https://www.gstatic.com/firebasejs/7.14.6/firebase-database.js"></script>
+
+        <script>
+            // Your web app's Firebase configuration
+            var firebaseConfig = {
+                apiKey: process.env.FIREBASE_RD_API_KEY,
+                authDomain: process.env.FIREBASE_RD_AUTH_DOMAIN,
+                databaseURL: process.env.FIREBASE_RD_DATABASE_URL,
+                projectId: process.env.FIREBASE_RD_PROJECT_ID,
+                storageBucket: process.env.FIREBASE_RD_STORAGE_BUCKET,
+                messagingSenderId: process.env.FIREBASE_RD_MESSAGING_SENDER_ID,
+                appId: process.env.FIREBASE_RD_APP_ID
+            };
+            // Initialize Firebase
+            firebase.initializeApp(firebaseConfig);
+
+            window.RD = firebase.database();
+        </script>
+
         <script defer src="{{ mix('js/app.js') }}"></script>
     </body>
 </html>
