9f96096e9610de3c4afdfe38dfd169d4734eedc9
grigor-aramyan <grigori.aramyan@gmail.com>
2020-06-12 21:00:58 +0400

    add private function to check connection between two users

--- app/Http/Controllers/UserConnectionController.php
+++ app/Http/Controllers/UserConnectionController.php
@@ -29,10 +29,8 @@ class UserConnectionController extends Controller
             return response()->json([ 'msg' => 'can\'t connect to yourself' ], 400);
         }
 
-        $check_one = UserConnection::whereRaw('connected_from = ? and connected_to = ?', array($current_user->id, $connected_to_id))->first();
-        $check_two = UserConnection::whereRaw('connected_from = ? and connected_to = ?', array($connected_to_id, $current_user->id))->first();
-        
-        if ($check_one || $check_two) {
+        $already_connected = $this->is_connected($current_user->id, $connected_to_id);
+        if ($already_connected) {
             return response()->json([ 'msg' => 'already connected' ], 200);
         }
 
@@ -47,4 +45,27 @@ class UserConnectionController extends Controller
             return response()->json([ 'msg' => 'error' ], 500);
         }
     }
+
+
+    // TODO: endpoint to return connected user ids
+
+    /**
+     * check if users with submitted ids are connected
+     * 
+     * @param int $id_1
+     * @param int $id_2
+     * @return boolean
+     */
+    private function is_connected($id_1, $id_2)
+    {
+        $check_one = UserConnection::whereRaw('connected_from = ? and connected_to = ?', array($id_1, $id_2))->first();
+        $check_two = UserConnection::whereRaw('connected_from = ? and connected_to = ?', array($id_2, $id_1))->first();
+        
+        if ($check_one || $check_two) {
+            return true;
+        }
+
+        return false;
+    }
+
 }
